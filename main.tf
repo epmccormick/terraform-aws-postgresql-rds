de302087916ffaf23d47b0da7e222724aab1baa6
Eric McCormick <eric@ericm.net>
2018-01-15 16:05:05 -0800

    Allow existing security group to be passed instead of creating new empty one

--- main.tf
+++ main.tf
@@ -7,17 +7,17 @@ terraform {
 
 #
 # Security group resources
+# EMC - Replaced with variable to allow passing existing Security Group
+
+#resource "aws_security_group" "postgresql" {
+#  vpc_id = "${var.vpc_id}"
 #
-
-resource "aws_security_group" "postgresql" {
-  vpc_id = "${var.vpc_id}"
-
-  tags {
-    Name        = "sgDatabaseServer"
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
-  }
-}
+#  tags {
+#    Name        = "sgDatabaseServer"
+#    Project     = "${var.project}"
+#    Environment = "${var.environment}"
+#  }
+#}
 
 #
 # RDS resources
@@ -43,7 +43,7 @@ resource "aws_db_instance" "postgresql" {
   copy_tags_to_snapshot      = "${var.copy_tags_to_snapshot}"
   multi_az                   = "${var.multi_availability_zone}"
   port                       = "${var.database_port}"
-  vpc_security_group_ids     = ["${aws_security_group.postgresql.id}"]
+  vpc_security_group_ids     = ["${var.security_group}"]
   db_subnet_group_name       = "${var.subnet_group}"
   parameter_group_name       = "${var.parameter_group}"
   storage_encrypted          = "${var.storage_encrypted}"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "security_group" {
+  default = "Unknown"
+}
+
 variable "project" {
   default = "Unknown"
 }
